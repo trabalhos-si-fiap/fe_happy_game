826a8f198db4f889b9680fe44ce0ccb89daeaf57
Murilo Godoi <godoi.murilo08@gmail.com>
2024-07-19 15:26:46 -0300

    mudança no background global, sessão sugira novos jogos na home finalizada

--- happy-game/src/app/page.tsx
+++ happy-game/src/app/page.tsx
@@ -2,18 +2,19 @@ import Header from "@/components/templates/header";
 import Image from "next/image";
 import primeiraImagem from "../../public/static/imgs/implementacao.png"
 import segundaImagem from "../../public/static/imgs/quem_somos-removebg-preview.png"
+import terceiraImagem from "../../public/static/imgs/novo_jogo-removebg-preview.png"
 import Link from "next/link";
 import Container from "@/components/templates/Container";
 import TextContainer from "@/components/templates/TextContainer";
 import LargeButton from "@/components/LargeButton";
 
+
 export default function Home() {
   return (
       <div>
          <Header></Header>
-         <div className="flex flex-col items-center gap-7">
+         <div className="flex flex-col items-center gap-20 ">
 
-          #Primeira parte do conteudo principal começa aqui
           <Container>
           <Image
               src={primeiraImagem}
@@ -22,29 +23,34 @@ export default function Home() {
               alt="Homem usando computador"
             />
             <TextContainer>
-              <h1 className="font-black text-3xl m-auto">Implementação de projeto</h1>
-              <p className=" text-justify">
+              <h1 className="font-black text-3xl m-auto text-black">Implementação de projeto</h1>
+              <p className=" text-justify text-black font-medium">
                 Somos um site focado em oferecer os melhores jogos digitais para aprendizado. 
                 Nosso objetivo é proporcionar uma experiência envolvente e educativa, onde cada 
                 jogo é cuidadosamente selecionado para promover o desenvolvimento cognitivo e 
                 habilidades essenciais de forma divertida e acessível.
               </p>
-              <LargeButton link={'/'} className={'bg-orange-400 border-orange-400'}>Jogue agora</LargeButton>
+              <LargeButton 
+              link={'/'} 
+              className={'bg-orange-400 border-orange-400 hover:text-orange-400'}>
+                Jogue agora
+              </LargeButton>
             </TextContainer>
           </Container>
-          #Primeira parte do conteudo principal termina aqui
-        
-          #Segunda parte do conteudo da pagina home começa aqui
           <Container>
             <TextContainer>
-              <h1 className="font-black text-3xl m-auto">Quem Somos</h1>
-              <p className="text-justify">Somos uma equipe dedicada à divulgação e criação de jogos educativos 
+              <h1 className="font-black text-3xl m-auto text-black">Quem Somos</h1>
+              <p className="text-justify text-black font-medium">Somos uma equipe dedicada à divulgação e criação de jogos educativos 
                 inovadores, reunindo paixão pela aprendizagem e compromisso com a inclusão. 
                 Nossa missão é oferecer experiências interativas que não apenas ensinam conceitos 
                 importantes,mas também celebram a diversidade e promovem um ambiente acolhedor para 
                 todos os jogadores, independentemente de suas origens ou habilidades.
               </p>
-              <LargeButton link={'/'} className={'bg-blue-800 border-blue-800'}>Ver sobre nós</LargeButton>
+              <LargeButton 
+              link={'/'} 
+              className={'bg-blue-800 border-blue-800  hover:text-blue-800'}>
+                Ver sobre nós
+              </LargeButton>
             </TextContainer>
             <Image
               src={segundaImagem}
@@ -53,7 +59,23 @@ export default function Home() {
               alt="Homem usando computador"
             />
           </Container>
-          #Segunda parte do conteudo da pagina home termina aqui 
+          <Container>
+            <Image
+            src={terceiraImagem}
+            width={500}
+            height={500}
+            alt="menino jogando videogame"
+            />
+            <TextContainer>
+              <h1 className="font-black text-3xl m-auto text-black">Sugira novos jogos</h1>
+              <p className="text-justify m-auto text-black font-medium">Clique no botão abaixo para sugerir um novo jogo.</p>
+              <LargeButton 
+              className={'bg-blue-800 border-blue-800 hover:text-blue-800'} 
+              link={"/"}>
+               Sugerir jogo
+              </LargeButton>
+            </TextContainer>
+          </Container>
          </div>
       </div>
   )
--- happy-game/src/components/LargeButton.tsx
+++ happy-game/src/components/LargeButton.tsx
@@ -4,7 +4,7 @@ export default function LargeButton(props: any){
     return (
         <Link href={props.link} 
               className={`text-center text-lg ${props.className ?? ''} px-4 py-4 rounded-md font-semibold cursor-pointer 
-              w-1/2 m-auto hover:bg-transparent border-4 ${props.className ?? ''}`}
+              w-1/2 m-auto hover:bg-transparent border-4 ${props.className ?? ''} ${props.className ?? ''}`}
                >
                 <span>{props.children}</span>
         </Link>
