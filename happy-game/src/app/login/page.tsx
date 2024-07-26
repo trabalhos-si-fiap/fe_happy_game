262a9b0f8431b199eda471633786bb2e798dacbf
Murilo Godoi <godoi.murilo08@gmail.com>
2024-07-26 14:20:58 -0300

    padronização login e cadastro

--- happy-game/src/app/login/page.tsx
+++ happy-game/src/app/login/page.tsx
@@ -1,51 +1,62 @@
+import CampoCadastro from "@/components/CampoCadastro";
+import CampoLogin from "@/components/CampoLogin";
 import Container from "@/components/templates/Container";
+import Link from "next/link";
 
-export default function LoginPage(){
-    return(
-        <div className="
+export default function LoginPage() {
+  return (
+    <div
+      className="
          text-white flex items-center justify-center h-screen
-         bg-orange-300">
-            <div className="
+         bg-orange-300"
+    >
+      <div
+        className="
              flex flex-col w-3/12 gap-10 items-center py-14 h-[60%] rounded-lg
-             bg-orange-400">
+             bg-orange-400"
+      >
+        <h1 className="text-4xl font-black">Login</h1>
 
-                <h1 className="text-4xl font-black">Login</h1>
+        <form className="gap-7 flex flex-col">
+          <CampoCadastro
+            tipo={"text"}
+            nome={"usuario_login"}
+            id={"id_login_usuario"}
+            placeholder={"Nome de usuario"}
+          />
+          <CampoCadastro
+            tipo={"text"}
+            nome={"senha_login"}
+            id={"id_login_senha"}
+            placeholder={"Digite sua senha"}
+          />
 
-                <form className="gap-7 flex flex-col">
-                    <div className="flex flex-col">
-                     <label 
-                     className="font-black text-[1.1rem]"
-                     htmlFor="login_id"
-                     >Username</label>
-                     <input 
-                      type="text" 
-                      name="login" 
-                      id="login_id"
-                      className="px-2 py-2 rounded-lg w-80"
-                      />
-                    </div>
-                    <div className="flex flex-col">
-                     <label 
-                     htmlFor="password_id"
-                     className="font-black text-[1.1rem]"
-                     >Password</label>
-                     <input 
-                      type="text" 
-                      name="password" 
-                      id="password_id"
-                      className=" px-2 py-2 rounded-lg w-80"
-                      />
-                    </div>                  
-                </form>
-
-                <div>
-                 <button className="
-                 bg-blue-500  w-80 px-12 py-2 rounded-lg 
-                hover:scale-105 hover:duration-200
-                text-lg font-semibold
-                ">Log in</button>
-                </div>
-            </div>
-        </div>
-    )
+          <div className="flex flex-col justify-center items-center gap-10">
+            <Link href={"/"}>
+            <button
+              className="
+                    bg-blue-500  w-80 px-12 py-2 rounded-lg 
+                    hover:bg-blue-400
+                    text-lg font-semibold
+                    "
+            >
+              Log in
+            </button>
+            </Link>
+            
+            <span>
+              Não possui uma conta?
+              <Link
+                href={"/cadastro"}
+                className="hover:text-blue-500 font-black"
+              >
+                {" "}
+                Cadastrar-se
+              </Link>
+            </span>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
